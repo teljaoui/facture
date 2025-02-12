44e0a7fdc6150341454c0ef0ce9f6d956fe033d5
teljaoui <teljaoui2004@gmail.com>
2025-02-12 16:43:54 +0100

    up

---
+++ resources/views/assets/asidebar.blade.php
@@ -0,0 +1,40 @@
+<div class="d-flex sidebar-header">
+        <img src="" alt="" class="sidebar-logo">
+        <a href="#" class="sidebar-logo">facture</a>
+    <button class="toggle-btn" type="button">
+        <i class="fa-solid fa-bars"></i>
+    </button>
+
+</div>
+<ul class="sidebar-nav">
+    <li class="sidebar-item">
+        <a href="/" class="sidebar-link">
+            <i class="fa-solid fa-chart-pie"></i>
+            <span>Tableau de bord</span>
+        </a>
+    </li>
+    <li class="sidebar-item">
+        <a href="{{route("clients")}}" class="sidebar-link">
+            <i class="fa-solid fa-user"></i>
+            <span>Clients</span>
+        </a>
+    </li>
+    <li class="sidebar-item">
+        <a href="#" class="sidebar-link">
+            <i class="fa fa-cube" aria-hidden="true"></i>
+            <span>Produits</span>
+        </a>
+    </li>
+    <li class="sidebar-item">
+        <a href="#" class="sidebar-link">
+            <i class="fa-solid fa-quote-left"></i>
+            <span>Devis</span>
+        </a>
+    </li>
+    <li class="sidebar-item">
+        <a href="#" class="sidebar-link">
+            <i class="fa-solid fa-file-invoice"></i>
+            <span>Factures</span>
+        </a>
+    </li>
+</ul>
