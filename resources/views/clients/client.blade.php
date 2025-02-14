c6a839c9b28a1b454fc54776474819f37791698e
teljaoui <teljaoui2004@gmail.com>
2025-02-14 11:26:18 +0100

    up

--- resources/views/clients/client.blade.php
+++ resources/views/clients/client.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head> 
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -11,30 +12,64 @@
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <title>Tableau de bord</title>
 </head>
+
 <body>
 
     <div class="wrapper">
         <aside id="sidebar">
-            @include("layout.asidebar")
+            @include('layout.asidebar')
         </aside>
         <div class="main">
             <header>
-                @include("layout.header")
+                @include('layout.header')
             </header>
-            <div class="container  my-5">
+            <div class="container my-5">
                 <div class="client-top d-flex align-items-center justify-content-between">
                     <form action="">
-                        <div class="form-group">
+                        <div class="form-group search-form">
                             <i class="fa-solid fa-magnifying-glass"></i>
                             <input type="text" class="form-control" placeholder="Rechercher">
                         </div>
                     </form>
                     <a href="" class="button">Ajouter un Client</a>
                 </div>
+                <div class="content">
+                    <div class="table-responsive py-1 px-3">
+                        <table class="table align-middle ">
+                            <thead>
+                                <tr>
+                                    <th>CLIENT</th>
+                                    <th class="text-center">FACTURE D'ACHAT</th>
+                                    <th class="text-center">ACTION</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <div class="d-flex align-items-center info">
+                                            <img src="assets/img/avatar.png" alt="Avatar" class="avatar me-3">
+                                            <div>
+                                                <a href="#">Mohamed Sadis</a><br>
+                                                <span>mohamedsadis@gmail.com</span>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td class="text-center">
+                                        <span class="badge">0</span>
+                                    </td>
+                                    <td class="text-center">
+                                        <a href="" class="btn-action edite"><i class="fas fa-edit"></i></a>
+                                        <a href="" class="btn-action"><i class="fas fa-trash text-danger"></i></a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    
+
 
     <script src="assets/js/script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
@@ -42,4 +77,5 @@
     </script>
 
 </body>
+
 </html>
